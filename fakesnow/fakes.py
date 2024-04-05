35598163075a2009f17d44f13754541b1001056f
ilkin Balkanay <ilkinulas@gmail.com>
2024-04-05 19:18:49 +0300

    add more tests  for object_agg

--- fakesnow/fakes.py
+++ fakesnow/fakes.py
@@ -206,7 +206,7 @@ class FakeSnowflakeCursor:
             .transform(transforms.timestamp_ntz_ns)
             .transform(transforms.float_to_double)
             .transform(transforms.integer_precision)
-            # TODO(selman): Broken, failes on CTAS queries with CASTs;
+            # TODO(selman): Broken, fails on CTAS queries with CASTs;
             # CREATE TABLE SOME_TABLE AS (
             #   SELECT
             #     R1 AS C1,
